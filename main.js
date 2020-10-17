8457f3d560b7e361d3c65fccc8388f3f0c1cfeb9
Jeremiah-Ng <72901442+Jeremiah-Ng@users.noreply.github.com>
2020-10-16 19:52:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function change(){
+document.getElementById("image1").src="https://www.kew.org/sites/default/files/2020-04/Water%20droplets%20on%20green%20leaf.jpg"
+}
+function change1(){
+    document.getElementById("image2").src="https://media.istockphoto.com/photos/tree-growth-three-steps-in-nature-and-beautiful-morning-lighting-picture-id956366756?k=6&m=956366756&s=612x612&w=0&h=sqi3kTfxi02Y-6xyyGmOjp9LI5ES_gipovhXenzaPPo="
+}
